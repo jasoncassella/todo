c6224e06ab796892391a41dd7962d260af551ea6
jason <jasoncassella1@gmail.com>
2022-01-14 20:23:30 -0500

    this is a start

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('hi');
+const addButton = document.querySelector('#add');
+
+class Todo {
+  constructor(title, details, date, completed, trash) {
+    this.title = title;
+    this.details = details;
+    this.date = date;
+    this.completed = completed;
+    this.trash = trash;
+  }
+}
+
+addButton.addEventListener('click', addTodo);
+
+let brushTeeth = new Todo('brush teeth', { location: 'home', priority: 'Low', due: 'dec 11 2021', comments: 'with colgate' });
+
+function addTodo() {
+  console.log('create new todo');
+}
