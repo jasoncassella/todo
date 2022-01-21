a56c62147ff53135a6f5b3f00ea6933e1fd07063
jason <jasoncassella1@gmail.com>
2022-01-21 14:40:48 -0500

    better naming

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
-let todos = [];
+const todos = [];
 
 const addButton = document.querySelector('#add');
 
 class Todo {
-  constructor(title, date, completed) {
+  constructor(title, date, isComplete) {
     this.title = title;
     this.date = date;
-    this.completed = completed;
+    this.isComplete = isComplete;
   }
 }
 
 addButton.addEventListener('click', addTodoPrompt);
 
 function addTodoPrompt() {
-  console.log('create new todo prompt');
+  // create new todo
+  addTodo('brush teeth', '1/21/22', false);
+}
+
+function addTodo(title, date, isComplete) {
+  const todo = new Todo(title, date, isComplete);
+  todos.push(todo);
+  updateContent();
 }
 
 function updateContent() {
-  const todoList = document.querySelector('.todo-list');
-  todos.forEach(todo => {
-    console.log(todo);
-  });
+  console.log(todos);
+  // const todoList = document.querySelector('.todo-list');
+  // todos.forEach(todo => {
+  //   console.log(todo);
+  // });
 }
 
 updateContent();
